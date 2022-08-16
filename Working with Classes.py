b4fc1f3c8b1357db93a245ac5ea67fa9cea20e38
Sangram Bahadur <sangramdhurve96@gmail.com>
2022-08-16 22:23:05 +0530

    Add files via upload

--- Working with Classes.py
+++ Working with Classes.py
@@ -1,4 +1,7 @@
 
+from calendar import weekday
+
+
 class Employee():
     num_of_emps = 0
     raise_amount = 1.04
@@ -14,22 +17,40 @@ class Employee():
 
     def apply_raise(self):
         self.pay = int(self.pay * Employee.raise_amount)# we also use "self instead of Employee here"
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
 
+    @classmethod
+    def form_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        Employee(first, last, pay)
+        cls(first, last, pay)
+
+
+    @staticmethod
+    def is_workingday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 emp_1 = Employee('Sangram', 'Bahadur', 20000000)
 emp_2 = Employee('Vansh', 'Bahadur', 20000000)
 
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-# for accessing the all details of emp_1
-print(emp_1.__dict__)# it will return all values in the form of dictionary
-# if I want to get all details of all employee then
-print(Employee.__dict__)
-# we can give manually amount
-Employee.raise_amount = 2.05# instead of Employee we can give specific employe name
-print(Employee.raise_amount)
-emp_2.raise_amount = 3.00
-print(emp_2.raise_amount)
-print(Employee.num_of_emps)
+import datetime
+
+my_date = datetime.date(2016, 7, 10)
+print(Employee.is_workingday(my_date))# it will return True or False based on day.
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'stive-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+new_emp_1 = Employee.form_string(emp_str_1)
+#print(new_emp_1.email)
+#print(new_emp_1.pay)
+
+# first, last, pay = emp_str_1.split('-')
+
+# emp_1.set_raise_amt(1.08)
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
